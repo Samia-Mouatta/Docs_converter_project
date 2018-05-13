e5e73985d6d7a2ff0e265141a78f9936d892a64d
Samia-Mouatta <samia.mouatta@gmail.com>
2018-05-13 20:49:43 +0200

    Initialize menu

--- FilesConverter/src/org/pdf/converter/Converter.java
+++ FilesConverter/src/org/pdf/converter/Converter.java
@@ -1,5 +1,13 @@
 package org.pdf.converter;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 public class Converter {
+	
+	public static void main(String[] args) throws MalformedURLException, IOException{
+		
+		new Window();
+	}
 
 }
---
+++ FilesConverter/src/org/pdf/converter/Window.java
@@ -0,0 +1,41 @@
+package org.pdf.converter;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.SwingConstants;
+
+public class Window extends JFrame{
+	
+	public Window() throws MalformedURLException, IOException{
+        JMenuBar menuBar = new JMenuBar();
+
+        String imageURL = "http://blogs.discovermagazine.com/" +
+            "drone360/wp-content/themes/discoverblog/images/" +
+            "gear_icon.png";
+
+        // Create a menu
+        JMenu menu = new JMenu("About");
+        BufferedImage image = ImageIO.read(new URL(imageURL));
+        menu.setHorizontalTextPosition(SwingConstants.CENTER);
+        menu.setVerticalTextPosition(SwingConstants.BOTTOM);
+        menu.setIcon(new ImageIcon(image));
+        menuBar.add(menu);
+
+		setTitle("Files Converter");
+		setJMenuBar(menuBar);
+		setSize(600,400);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+
+}
