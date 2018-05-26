adbefb3d130a568333ee7dff29906766622b7b93
Samia-Mouatta <samia.mouatta@gmail.com>
2018-05-26 18:43:57 +0200

    update bar menu

--- FilesConverter/src/org/pdf/converter/Window.java
+++ FilesConverter/src/org/pdf/converter/Window.java
@@ -1,43 +1,92 @@
 package org.pdf.converter;
 
-import java.awt.image.BufferedImage;
+import java.awt.FlowLayout;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 public class Window extends JFrame{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	String aboutImg = "Resources/images/about.png";
+	String optionsImg = "Resources/images/options.png";
+	String helpImg = "Resources/images/help.png";
+	String exitImg = "Resources/images/exit.png";
+	
+	JPanel panelCenter;
+	JMenuBar menuBar;
+   	JMenu about,option,help,exit;
+   	JButton button1,button2;
+	
 	public Window() throws MalformedURLException, IOException{
 		
-        JMenuBar menuBar = new JMenuBar();
-
-        String imageURL = "http://blogs.discovermagazine.com/" +
-            "drone360/wp-content/themes/discoverblog/images/" +
-            "gear_icon.png";
+        createBarMenu(about,help,exit);
         
-
-        // Create a menu
-        JMenu menu = new JMenu("About");
-        BufferedImage image = ImageIO.read(new URL(imageURL));
-        menu.setHorizontalTextPosition(SwingConstants.CENTER);
-        menu.setVerticalTextPosition(SwingConstants.BOTTOM);
-        menu.setIcon(new ImageIcon(image));
-        menuBar.add(menu);
-
+        createPanel();
+        
+        //parameters
 		setTitle("Files Converter");
-		setJMenuBar(menuBar);
 		setSize(600,400);
 		setVisible(true);
+		setJMenuBar(menuBar);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 	
+	public void createBarMenu(JMenu menu1,JMenu menu2,JMenu menu3) throws MalformedURLException, IOException{
+	
+		menuBar = new JMenuBar();
+		
+		about = new JMenu("About");
+		createImage(about,aboutImg);
+		menuBar.add(about);
+		
+		option = new JMenu("Options");
+		createImage(option,optionsImg);
+		menuBar.add(option);
+		
+		help = new JMenu("Help");
+		createImage(help,helpImg);
+		menuBar.add(help);
+		
+		
+		exit = new JMenu("Exit");
+		createImage(exit,exitImg);
+		menuBar.add(exit);
+		
+	}
+	
+	// create image associated to the item menu
+	public void createImage(JMenu menu,String imageURL) throws MalformedURLException, IOException{
+		
+        //BufferedImage image = ImageIO.read(new URL(imageURL));
+        menu.setHorizontalTextPosition(SwingConstants.CENTER);
+        menu.setVerticalTextPosition(SwingConstants.BOTTOM);
+        menu.setIcon(new ImageIcon(imageURL));
+        menuBar.add(menu);
+        
+	}
+	
+	//create panel
+	public void createPanel(){
+		
+        button1 =  new JButton("PDF TO WORD");
+        button2 =  new JButton("WORD TO PDF");
+        panelCenter = new JPanel();
+        add(panelCenter);
+        panelCenter.setLayout(new FlowLayout());
+        panelCenter.add(button1);
+        panelCenter.add(button2);
+	}
 
 }
